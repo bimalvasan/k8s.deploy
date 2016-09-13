52d79da7a30993bb47f901e4c4b8af94e09bbf5b
Bimal <bimalvasan@gmail.com>
2016-09-13 17:19:43 +0530

    Included Builder

---
+++ src/main/java/org/jenkinsci/plugins/k8s/deploy/DeploymentBuilder.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package org.jenkinsci.plugins.k8s.deploy;
+
+import java.io.IOException;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.AbstractProject;
+import hudson.model.Build;
+import hudson.model.BuildListener;
+import hudson.model.FreeStyleProject;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Builder;
+
+/**
+ * @author bimal
+ *
+ */
+public class DeploymentBuilder extends Builder {
+	private long time;
+	
+	@DataBoundConstructor
+	public DeploymentBuilder(long time){
+		this.time = time;
+	}
+
+	public long getTime() {
+		return time;
+	}
+
+	public void setTime(long time) {
+		this.time = time;
+	}	
+	
+	@Override
+	public boolean perform(Build<?, ?> build, Launcher launcher, BuildListener listener)
+			throws InterruptedException, IOException {
+		listener.getLogger().print("Sleeping for " + this.time + " ms.");
+		Thread.sleep(time);
+		return true;
+	}
+	
+	/***
+	 * 
+	 * @author bimal	 *
+	 */
+	@Extension
+	public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+
+		@Override
+		public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+			return jobType == FreeStyleProject.class;
+		}
+
+		@Override
+		public String getDisplayName() {
+			return "Deployment Builder";
+		}		
+	}
+}
